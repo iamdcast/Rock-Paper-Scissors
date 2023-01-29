18dfcc762f234a3c9c89ac4586a0227da36b9f7e
david.dev <58799111+iamdcast@users.noreply.github.com>
2023-01-29 09:37:29 -0800

    Files uploaded
    Initial upload

---
+++ README.md
@@ -0,0 +1,61 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Netlify Sit : (https://rock-paper-scissors-bydcast.netlify.app/)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- JS 
+
+### What I learned
+
+I used more CSS animations from Animate.style. I will start using more for my projects.
+
+
+```css
+.hands img:hover{
+filter: drop-shadow(0 0 0.75rem #E7F6F2);
+animation-name: tada;
+animation-duration: 1.5s;
+animation-timing-function: ease-in-out;
+}
+```
+
+### Continued development
+
+Continue more projects
+
+
+
+
+
+## Author
+
+- Frontend Mentor - [@iamdcast](https://www.frontendmentor.io/profile/iamdcast)
+- Twitter - [@iamdcast](https://www.twitter.com/iamdcast)
+
+
---
+++ js/main.js
@@ -0,0 +1,59 @@
+function botChoice (){
+    let choices = ["rock","paper","scissors"]
+    let bot = choices[Math.floor(Math.random() * 3)];
+    return bot;
+}
+
+let score = 0;
+
+function scoreTracker (newScore){
+    score = newScore
+    document.querySelector('.ScoreTracker h1').innerText = score;
+}
+
+function results (hand, botPick){
+    document.querySelector('#userPick').removeAttribute("style");
+    document.querySelector('.userhand div').removeAttribute("style");
+    document.querySelector('#userPick').style.animationName="none";
+     if (hand === "rock" && botPick === "scissors" || hand === "scissors" && botPick === "paper" || hand === "paper" && botPick === "rock" ) {
+
+        document.querySelector('#result').innerText = "You Win!";
+        document.querySelector('#userPick').style.filter = "drop-shadow(0 0 1.5rem #E7F6F2)";
+        document.querySelector('.userhand div').style.transform = "scalex(-1) rotate(-35deg)"
+        document.querySelector('#userPick').style.animationName = "tada";
+        document.querySelector('#userPick').style.animationDuration = "1.5s";
+        document.querySelector('#userPick').style.animationIterationCount = "infinite";
+        scoreTracker(score+1);
+
+    } else if ( hand === botPick){
+        document.querySelector('#result').innerText = "Draw!";
+    } else {document.querySelector('#result').innerText = "You Lost!";
+    document.querySelector('#userPick').style.animationName="hinge";
+    document.querySelector('#userPick').style.animationDuration="3s";}
+}
+
+
+
+const userChoice = (hand) => {
+    console.log(hand);
+    //hide the current page
+    document.querySelector('.hands').style.display = "none";
+
+    //show the new page 
+    document.querySelector('.results').style.display = "flex";
+
+    //show the user choice with bot choice
+
+    let botPick = botChoice();
+
+    document.querySelector('#userPick').src = `img/${hand}.png`;
+    document.querySelector('#botPick').src = `img/${botPick}.png`;
+    //show the winner
+   results(hand, botPick)
+}
+
+
+function playagain (){
+    document.querySelector('.results').style.display = "none";
+    document.querySelector('.hands').style.display = "flex";
+}
